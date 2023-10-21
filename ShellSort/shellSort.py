ded80e86b0347f5ff008319116aeb508c49ddb84
Emilio <emiliocaballero@ciencias.unam.mx>
2023-10-20 21:31:59 -0600

    se hizo completo shell sort

--- ShellSort/shellSort.py
+++ ShellSort/shellSort.py
@@ -1,18 +1,20 @@
 import random
 
 # Crear un arreglo vacío
-arreglo = []
-arr_hib=[]
 
-# Llenar el arreglo con 35 números enteros aleatorios entre 1 y 100
-for i in range(35):
-    numero_entero = random.randint(1, 100)  # Se puede ajustar segun los gustos
-    arreglo.append(numero_entero)
-    arr_hib.append(numero_entero)
+#Descomentar o comentar esta seccion de codigo si quiere Shell sort mas rapido
+arreglo = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,21,20,23,22,24,25,26,27,28,29,30,31,32,33,34,35]
+arr_hib = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,21,20,23,22,24,25,26,27,28,29,30,31,32,33,34,35]
+
+#Descomentar o comentar esta seccion del codigo si quiere Hibbrad mas rapido
+"""arreglo = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35]
+arr_hib = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35]"""
+
+
 
 def shell_sort(arr):
     n = len(arr)
-    gap = n// 2
+    gap = n // 2
     iter=0
     while gap > 0:
         for i in range(gap, n):
@@ -22,8 +24,8 @@ def shell_sort(arr):
                 arr[j] = arr[j - gap]
                 j -= gap
             arr[j] = temp
-        iter+=1
-        print("En la iteracion",iter,"el arreglo es",arr)
+            iter+=1
+            print("En la iteracion",iter,"el arreglo es",arr)
         gap //= 2
 
 
@@ -44,8 +46,8 @@ def hibbard_shell_sort(arr):
                 arr[j] = arr[j - gap]
                 j -= gap
             arr[j] = temp
-        iter+=1
-        print("En la iteracion",iter,"el arreglo es",arr)
+            iter+=1
+            print("En la iteracion",iter,"el arreglo es",arr)     
         gap = (gap - 1) // 2
 
 # Ejemplo de uso
